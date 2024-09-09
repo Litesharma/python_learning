6335d3b68c589dd1fdbbb553b2bff105f4fb6883
Litesh <liteshsharma9466@gmail.com>
2024-09-09 05:53:56 +0530

    problems on text files (write ,read ,append etc)

--- practise/practise_9.py
+++ practise/practise_9.py
@@ -1,1 +1,41 @@
-''''''
+'''Write a program ro get roll no. names and marks of the students of a class from the user and stor these details in a file clled "marks.txt'''
+
+# a=int(input("enter total number of students : "))
+# f=open("practise/mark.txt","w")
+# for i in range(0,a):
+#     print(f"enter details of student {i+1}")
+#     r=int(input("enter roll no. : "))
+#     m=int(input("enter marks : "))
+#     n=input("enter name : ")
+#     str=f'{r},{n},{m}\n'
+#     f.write(str)
+
+# f.close()
+
+'''write a program to read a text file line by line and display each word separated by a '#'  '''
+
+# f=open("classes//new.txt","r")
+# str=f.read()
+# for i in str:
+#     if(i==' '):
+#         a='#'
+#         print(a,end='')
+#     else:
+#         print(i,end='')
+# f.close()
+
+
+'''write a program to read a text file and display the count of vowels ans consonants in the file '''
+
+# f=open("classes//new.txt","r")
+# str=f.read()
+# count_v=0
+# count_c=0
+# for i in str:
+#     if(i.lower() in 'aeiou'):
+#         count_v+=1
+#     elif(i.isalpha()):
+#         count_c+=1
+# print(f"total number of vowels are {count_v}\ntotal number of consonents are {count_c}")
+
+# f.close()
