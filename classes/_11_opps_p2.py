a55e8af0fff150bf941929ba6e824e7180824a4e
Litesh <liteshsharma9466@gmail.com>
2024-09-12 05:24:18 +0530

    class method , property method

--- classes/_11_opps_p2.py
+++ classes/_11_opps_p2.py
@@ -57,32 +57,78 @@ Multiple inheritance
 super() method: it is used to access the methods of the parent class
 
 '''
-class Car:
-    def __init__(self,color):
-        self.color=color
-    @staticmethod
-    def start():
-        print("car started ")
-    @staticmethod
-    def stop():
-        print("car stop ")
+# class Car:
+#     def __init__(self,color):
+#         self.color=color
+#     @staticmethod
+#     def start():
+#         print("car started ")
+#     @staticmethod
+#     def stop():
+#         print("car stop ")
 
-class NewCar(Car):
-    def __init__(self,brand,color):               
-        super().__init__(color)
-        self.brand=brand
+# class NewCar(Car):
+#     def __init__(self,brand,color):               
+#         super().__init__(color)
+#         self.brand=brand
 
-class CarModel(NewCar):
-    def __init__(self,model,brand,color):
-        super().__init__(brand,color)
-        self.model=model
+# class CarModel(NewCar):
+#     def __init__(self,model,brand,color):
+#         super().__init__(brand,color)
+#         self.model=model
 
-c=CarModel("320B4U","BMW","green")
-print(c.model)
-print(c.brand)
-print(c.color)
+# c=CarModel("320B4U","BMW","green")
+# print(c.model)
+# print(c.brand)
+# print(c.color)
 
-#print(c.brand)   now here i want to access the brand name but i can't so i will use super() method
-c.start()
-c.stop()
+# #print(c.brand)   now here i want to access the brand name but i can't so i will use super() method
+# c.start()
+# c.stop()
 
+
+'''
+Class method : it helps to change the class attributes in methods 
+
+'''
+# class person:
+#     name="bhola"
+#     def changeName(self,name):
+#         # self.name=name                  # this will not change the class's name
+#         # person.name=name                # this will change the name 
+#         # self.__class__.name=name        # this will change the name
+# p1=person()
+# p1.changeName("chintu")
+# print(p1.name)
+# print(person.name)
+
+# class person:
+#     name="bhola"
+#     @classmethod              # decorator
+#     def changeName(cls,name):
+#         cls.name=name
+
+# p1=person()
+# p1.changeName("chintu")
+# print(p1.name)
+# print(person.name)
+
+
+
+'''
+@property attribute : converts method to attrubute with updated values
+
+
+'''
+
+# class Table:
+#     def __init__(self,n):
+#         self.n=n
+#         # self.t=n*2                  # now we have changes the value to 5 but it will again print 8
+#     @property
+#     def t(self):
+#         return self.n*2               # now it will return the updated value 10
+    
+# s=Table(4)
+# s.n=5                  
+# print(s.t)
